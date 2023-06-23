62456aeed8277f12ade694a7f04bc3e73a8ad564
paudel54 <2sansrit@gmail.com>
2023-06-23 10:24:35 +0545

    added bootstrap CDN's

---
+++ axios/src/Pages/Post/Post.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Post.css";
+const Post = () => {
+  return (
+    <div className="post__wrapper">
+      <div className="container">
+        <h1>Single container post</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
---
+++ axios/src/Pages/Posts/Posts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Posts.css";
+const Posts = () => {
+  return <div className="posts">post</div>;
+};
+
+export default Posts;
